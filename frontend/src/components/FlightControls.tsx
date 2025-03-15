b8dcf796f31a4e7c260e2ebba3976a15568e59d2
Linus Bolls <linus.bolls@gmail.com>
2025-03-16 00:33:28 +0100

    style: ground control page

--- frontend/src/components/FlightControls.tsx
+++ frontend/src/components/FlightControls.tsx
@@ -23,6 +23,33 @@ export interface FlightControlProps {}
 export const FlightControlss: React.FC<FlightControlProps> = () => {
   const { data, isLoading, error } = useCurrentFlight();
 
+  const onTakeoffClick = async () => {
+    if (!data?.id) return;
+
+    const password = prompt('Please enter "takeoff" to confirm:');
+    if (
+      typeof password === "string" &&
+      password.trim().toLowerCase() !== "takeoff"
+    ) {
+      alert("Invalid input, aborting takeoff.");
+      return;
+    }
+
+    const takeOffRes = await fetch(`/api/flights/${data?.id}/events`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        sent: Date.now(),
+        command: "start",
+      }),
+    });
+    if (!takeOffRes.ok) {
+      alert("Error: Failed to send takeoff command");
+    }
+  };
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -34,25 +61,24 @@ export const FlightControlss: React.FC<FlightControlProps> = () => {
   }
 
   return (
-    <div>
-      <h1>Flight ID: {data.id}</h1>
-      <button
-        onClick={async () => {
-          await fetch(`/api/flights/${data.id}/events`, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              sent: Date.now(),
-              command: "start",
-            }),
-          });
-        }}
-      >
-        Takeoff
-      </button>
-    </div>
+    <>
+      <div className="inner-left-bottom">
+        <h1>Ground Control</h1>
+        <p>Flight ID: {data.id}</p>
+        <div style={{ display: "flex", gap: "1rem", marginTop: "1rem" }}>
+          <button onClick={onTakeoffClick} className="button">
+            Takeoff
+          </button>
+          <a
+            target="_blank"
+            href={`/api/flights/${data.id}/logs`}
+            className="button"
+          >
+            View Logs
+          </a>
+        </div>
+      </div>
+    </>
   );
 };
 
